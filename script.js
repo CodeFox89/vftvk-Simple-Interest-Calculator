e32c474b84f7750ca8ceefe9dba5e3071169ac61
CodeFox89 <85134052+CodeFox89@users.noreply.github.com>
2021-06-03 15:04:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ function compute()
     var amount = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
     
-    if (principal < 0) {
+    if (isNaN(principal) || principal < 0) {
         alert("Enter a positive number");
+    } else {
+        document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+amount+"</mark>, \<br\>in the year <mark>"+year+"</mark> \<br\>"
     }
-    
-    document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"</mark>%\<br\>You will receive an amount of <mark>"+amount+",</mark> \<br\>in the year <mark>"+year+"</mark> \<br\>"
     
 }
 
